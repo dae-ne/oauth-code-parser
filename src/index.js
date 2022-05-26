b290ee5ae9e7721f21d2fea668ea534cbd8c46ce
Rafał Czajka <rafal.czajka@fortedigital.no>
2022-05-26 17:54:38 +0200

    Production ready app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3001;
-
-app.listen(PORT, () => {
-  console.log('\nServer is ready:', `\x1b[34mhttp://localhost:${PORT}`);
-});
+if (process.argv.includes('--dev')) {
+  const PORT = 3001;
+  app.listen(PORT, () => {
+    console.log('\nServer is running:', `\x1b[34mhttp://localhost:${PORT}`);
+  });
+}
 
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.sendFile(__dirname + '/static/index.html');
 });
+
+module.exports = app;
