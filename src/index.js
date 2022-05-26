6c02ae0581558e296a597c8fd86d5c09fe640882
dae-ne <rafalcz29@gmail.com>
2022-05-26 18:26:38 +0200

    Remove configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
 
-if (process.argv.includes('--dev')) {
-  const PORT = 3001;
-  app.listen(PORT, () => {
-    console.log('\nServer is running:', `\x1b[34mhttp://localhost:${PORT}`);
-  });
-}
+const PORT = 3001;
 
 app.use(express.static(__dirname + '/static'));
 
@@ -14,4 +9,6 @@ app.get('/', (_, res) => {
   res.sendFile(__dirname + '/static/index.html');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log('\nServer is running:', `\x1b[34mhttp://localhost:${PORT}`);
+});
