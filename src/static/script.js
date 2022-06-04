1a09c784f9ca8a70851f67141c789d9defb0564c
dae-ne <rafalcz29@gmail.com>
2022-06-04 21:44:02 +0200

    Add a toast component

--- src/static/script.js
+++ src/static/script.js
@@ -6,6 +6,18 @@ const disableButton = () => {
   button.disabled = true;
 };
 
+const showToast = (message, type = 'message') => {
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: 'bottom',
+    position: 'right',
+    stopOnFocus: true,
+    className: `toast toast-${type}`
+  }).showToast();
+};
+
 const parseCodeFromUrl = () => {
   const codeParagraph = document.querySelector(CODE_QUERY);
   const url = new URL(window.location.href);
@@ -16,12 +28,14 @@ const parseCodeFromUrl = () => {
   if (!parsedCode) {
     disableButton();
     codeParagraph.classList.add('error');
+    showToast('Code cannot be parsed', 'error');
   }
 };
 
 const copyToClipboard = () => {
   const code = document.querySelector(CODE_QUERY).textContent;
   navigator.clipboard.writeText(code);
+  showToast('Copied to clipboard');
 };
 
 const button = document.querySelector(BUTTON_QUERY);
