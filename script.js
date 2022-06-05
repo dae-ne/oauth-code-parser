e775fbaa44fb55b57c489e262fae3e5fe98399b6
dae-ne <65031647+dae-ne@users.noreply.github.com>
2022-06-05 12:23:06 +0000

    Update gh-pages to output generated at f352f6a

---
+++ script.js
@@ -0,0 +1,43 @@
+const BUTTON_QUERY = 'button';
+const CODE_QUERY = 'p';
+
+const disableButton = () => {
+  const button = document.querySelector(BUTTON_QUERY);
+  button.disabled = true;
+};
+
+const showToast = (message, type = 'message') => {
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: 'bottom',
+    position: 'right',
+    stopOnFocus: true,
+    className: `toast toast-${type}`
+  }).showToast();
+};
+
+const parseCodeFromUrl = () => {
+  const codeParagraph = document.querySelector(CODE_QUERY);
+  const url = new URL(window.location.href);
+  const parsedCode = url.searchParams.get('code');
+
+  codeParagraph.textContent = parsedCode ?? 'parsing error';
+  
+  if (!parsedCode) {
+    disableButton();
+    codeParagraph.classList.add('error');
+    showToast('Code cannot be parsed', 'error');
+  }
+};
+
+const copyToClipboard = () => {
+  const code = document.querySelector(CODE_QUERY).textContent;
+  navigator.clipboard.writeText(code);
+  showToast('Copied to clipboard');
+};
+
+const button = document.querySelector(BUTTON_QUERY);
+parseCodeFromUrl();
+button.addEventListener('click', copyToClipboard);
