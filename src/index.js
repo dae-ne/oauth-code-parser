5a306dda4be5925e727a2f258feecdf8290ffe97
dae-ne <rafalcz29@gmail.com>
2022-06-05 14:42:26 +0200

    Fix colors in console

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.get('/', (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('\nServer is running:', `\x1b[34mhttp://localhost:${PORT}`);
+  console.log('\nServer is running: \x1b[34m%s\x1b[0m', `http://localhost:${PORT}`);
 });
